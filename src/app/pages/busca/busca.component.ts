cdf93c5e832cec0571d81efaf9731eb2cfd873a8
diegocgayoso <diegoc.gayoso@gmail.com>
2023-08-02 14:28:59 -0300

    Aula 1.4

--- src/app/pages/busca/busca.component.ts
+++ src/app/pages/busca/busca.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PassagensService } from 'src/app/core/services/passagens.service';
 
 @Component({
   selector: 'app-busca',
   templateUrl: './busca.component.html',
   styleUrls: ['./busca.component.scss']
 })
-export class BuscaComponent {
+export class BuscaComponent implements OnInit {
+  constructor(private passagensService: PassagensService) { }
+  ngOnInit(): void {
+    const buscaPadrao = {
+      data: new Date().toISOString,
+      pagina: 1,
+      porPagina: 25,
+      somenteIda: false,
+      passageirosAdultos: 1,
+      tipo: "Executiva"
+    }
 
+    this.passagensService.getPassagens(buscaPadrao).subscribe(
+      res => console.log(res)
+    )
+  }
 }
