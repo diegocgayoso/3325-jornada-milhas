904c06f4bef86682d6f5d83dc76ce316ee4833a9
diegocgayoso <diegoc.gayoso@gmail.com>
2023-08-02 14:32:23 -0300

    Aula 1.5

--- src/app/core/services/passagens.service.ts
+++ src/app/core/services/passagens.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Resultado } from '../types/type';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,8 @@ export class PassagensService {
   constructor(
     private httpClient: HttpClient
   ) { }
-  getPassagens(search: any){
+  getPassagens(search: any) : Observable<Resultado>{
     const params = search;
-    return this.httpClient.get(this.apiUrl + '/passagem/search', {params})
+    return this.httpClient.get<Resultado>(this.apiUrl + '/passagem/search', {params})
   }
 }
