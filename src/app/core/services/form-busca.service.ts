4f9ad6552473e06f150e398bd46ee87d990d7137
diegocgayoso <diegoc.gayoso@gmail.com>
2023-08-03 09:34:30 -0300

    Implementando desafio de criar CompanhiaComponent

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -26,7 +26,8 @@ export class FormBuscaService {
       bebes: new FormControl(1),
       dataIda: new FormControl(null, [Validators.required]),
       dataVolta,
-      conexoes: new FormControl(null)
+      conexoes: new FormControl(null),
+      companhias: new FormControl(null),
     })
     somenteIda.valueChanges.subscribe(somenteIda => {
       if(somenteIda){
@@ -97,11 +98,15 @@ export class FormBuscaService {
     if (dataVoltaControl.value) {
       dadosBusca.dataVolta = dataVoltaControl.value.toISOString();
     }
-
     const conexoesControl = this.obterControle<number>('conexoes');
     if(conexoesControl.value){
       dadosBusca.conexoes = conexoesControl.value;
     }
+    const companhiasControl = this.obterControle<number[]>('companhias');
+    if(companhiasControl.value){
+      dadosBusca.companhiasId = companhiasControl.value
+    }
+
     return dadosBusca
   }
 
