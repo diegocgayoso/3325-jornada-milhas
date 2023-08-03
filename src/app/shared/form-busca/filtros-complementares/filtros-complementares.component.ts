111064cae907b7b3491488f66a267c4765b1c56a
diegocgayoso <diegoc.gayoso@gmail.com>
2023-08-03 16:42:55 -0300

    Aula 5.1

--- src/app/shared/form-busca/filtros-complementares/filtros-complementares.component.ts
+++ src/app/shared/form-busca/filtros-complementares/filtros-complementares.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuscaService } from 'src/app/core/services/form-busca.service';
+import { PassagensService } from 'src/app/core/services/passagens.service';
 
 @Component({
   selector: 'app-filtros-complementares',
@@ -8,7 +9,10 @@ import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 })
 export class FiltrosComplementaresComponent {
   @Output() realizarBusca = new EventEmitter();
-  constructor(public formBuscaService: FormBuscaService){}
+  constructor(
+    public formBuscaService: FormBuscaService,
+    private passagemService: PassagensService
+  ){}
 
   busca(){
     if(!this.formBuscaService.formEstaValido){
@@ -21,5 +25,13 @@ export class FiltrosComplementaresComponent {
       return
     }
     this.realizarBusca.emit(this.formBuscaService.obterDadosBusca())
+  }
+  limparFiltros(){
+    this.formBuscaService.formBusca.patchValue({
+      conexoes: null,
+      companhias: null,
+      precoMin: this.passagemService.precoMin,
+      precoMax: this.passagemService.precoMax,
+    })
   }
 }
